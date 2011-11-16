269a5c7b51c0e811c4f1e5ec559390e3ede0aa41
jplagostena <jplagostena@10ae812c-546e-796a-a440-56309e2bfe1d>
2011-11-16 05:09:52 +0000

    Pbx Users y linkeo con Paranoid Users

--- trunk/lib/model/doctrine/sfDoctrineGuardPlugin/base/BasesfGuardUser.class.php
+++ trunk/lib/model/doctrine/sfDoctrineGuardPlugin/base/BasesfGuardUser.class.php
@@ -15,8 +15,10 @@
  * @property boolean $is_active
  * @property boolean $is_super_admin
  * @property timestamp $last_login
+ * @property integer $pbxuser_id
  * @property Doctrine_Collection $Groups
  * @property Doctrine_Collection $Permissions
+ * @property PBXUser $PBXUser
  * @property Doctrine_Collection $sfGuardUserPermission
  * @property Doctrine_Collection $sfGuardUserGroup
  * @property sfGuardRememberKey $RememberKeys
@@ -32,8 +34,10 @@
  * @method boolean               getIsActive()              Returns the current record's "is_active" value
  * @method boolean               getIsSuperAdmin()          Returns the current record's "is_super_admin" value
  * @method timestamp             getLastLogin()             Returns the current record's "last_login" value
+ * @method integer               getPbxuserId()             Returns the current record's "pbxuser_id" value
  * @method Doctrine_Collection   getGroups()                Returns the current record's "Groups" collection
  * @method Doctrine_Collection   getPermissions()           Returns the current record's "Permissions" collection
+ * @method PBXUser               getPBXUser()               Returns the current record's "PBXUser" value
  * @method Doctrine_Collection   getSfGuardUserPermission() Returns the current record's "sfGuardUserPermission" collection
  * @method Doctrine_Collection   getSfGuardUserGroup()      Returns the current record's "sfGuardUserGroup" collection
  * @method sfGuardRememberKey    getRememberKeys()          Returns the current record's "RememberKeys" value
@@ -48,8 +52,10 @@
  * @method sfGuardUser           setIsActive()              Sets the current record's "is_active" value
  * @method sfGuardUser           setIsSuperAdmin()          Sets the current record's "is_super_admin" value
  * @method sfGuardUser           setLastLogin()             Sets the current record's "last_login" value
+ * @method sfGuardUser           setPbxuserId()             Sets the current record's "pbxuser_id" value
  * @method sfGuardUser           setGroups()                Sets the current record's "Groups" collection
  * @method sfGuardUser           setPermissions()           Sets the current record's "Permissions" collection
+ * @method sfGuardUser           setPBXUser()               Sets the current record's "PBXUser" value
  * @method sfGuardUser           setSfGuardUserPermission() Sets the current record's "sfGuardUserPermission" collection
  * @method sfGuardUser           setSfGuardUserGroup()      Sets the current record's "sfGuardUserGroup" collection
  * @method sfGuardUser           setRememberKeys()          Sets the current record's "RememberKeys" value
@@ -110,6 +116,10 @@ abstract class BasesfGuardUser extends sfDoctrineRecord
         $this->hasColumn('last_login', 'timestamp', null, array(
              'type' => 'timestamp',
              ));
+        $this->hasColumn('pbxuser_id', 'integer', 4, array(
+             'type' => 'integer',
+             'length' => 4,
+             ));
 
 
         $this->index('is_active_idx', array(
@@ -132,6 +142,10 @@ abstract class BasesfGuardUser extends sfDoctrineRecord
              'refClass' => 'sfGuardUserPermission',
              'local' => 'user_id',
              'foreign' => 'permission_id'));
+
+        $this->hasOne('PBXUser', array(
+             'local' => 'pbxuser_id',
+             'foreign' => 'id'));
 
         $this->hasMany('sfGuardUserPermission', array(
              'local' => 'id',
