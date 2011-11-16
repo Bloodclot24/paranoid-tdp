9c16be7973ac1be25929d3fd31446fa00333034b
jplagostena <jplagostena@10ae812c-546e-796a-a440-56309e2bfe1d>
2011-11-16 05:10:46 +0000

    Pbx Users y linkeo con Paranoid Users

---
+++ apps/admin/modules/pbx_user/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/pbx_userGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/pbx_userGeneratorHelper.class.php';
+
+/**
+ * pbx_user actions.
+ *
+ * @package    paranoid-web
+ * @subpackage pbx_user
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class pbx_userActions extends autoPbx_userActions
+{
+}
