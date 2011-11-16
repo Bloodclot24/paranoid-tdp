9fcbcbd2f31e22d081a7495063ad4b4dba47afb9
jplagostena <jplagostena@10ae812c-546e-796a-a440-56309e2bfe1d>
2011-11-16 05:12:27 +0000

    Pbx Users y linkeo con Paranoid Users

---
+++ lib/filter/doctrine/PBXUserFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * PBXUser filter form.
+ *
+ * @package    paranoid-web
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class PBXUserFormFilter extends BasePBXUserFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/form/doctrine/PBXUserForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * PBXUser form.
+ *
+ * @package    paranoid-web
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class PBXUserForm extends BasePBXUserForm
+{
+  public function configure()
+  {
+  }
+}
